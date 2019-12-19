2bd82811c317379869ba99a29f9d6fe869ae95cc
greg baker <gregbakerstl@gmail.com>
2019-12-19 16:03:04 -0600

    shellcheck

--- reverse_dns_lookup.sh
+++ reverse_dns_lookup.sh
@@ -5,9 +5,9 @@ check_prips() {
 # check prips dependency
 
 	service='prips'
-	service_path=$(which prips)
+	service_path=$(which "$service")
 
-	if [ -z $service_path ]; then
+	if [ -z "$service_path" ]; then
 		echo 'Please install prips'
 		echo 'sudo apt install -y prips'
 	fi
@@ -16,9 +16,9 @@ check_prips() {
 
 expand_networks() {
 # return reverse CIDR notation 
-	cidr_networks=$@
-	for network in $cidr_networks; do
-		prips $network|sort -R
+	cidr_networks=("$@")
+	for network in ${cidr_networks[*]}; do
+		prips "$network"|sort -R
 	done
 }
 
@@ -26,11 +26,11 @@ expand_networks() {
 lookup_ip() {
 # given an IP, ask public recursive server to return fqdn
 	ip=$1
-	domain=$(host $ip 8.8.8.8)
+	domain=$(host "$ip" 8.8.8.8)
 	echo "$ip $domain"|
 	grep -v 'not found'|
 	grep 'ointer'|
-	awk {'print $5'}|
+	awk '{print $5}'|
 	sed 's/.$//g'
 }
 
@@ -39,21 +39,21 @@ main() {
 	check_prips
 
 	# this computers core count
-	cores=$(grep proces /proc/cpuinfo|wc -l)
+	cores=$(grep -c proces /proc/cpuinfo)
 
 	# all the IPs to check
-	ip_list=$(expand_networks $cidr_networks)
+	ip_list=$(expand_networks "${cidr_networks[*]}")
 
 	# MULTITHREAD DNS lookups!
 	echo "$ip_list"|
-	xargs -P $cores -n 1 -I {} bash -c 'lookup_ip "$1"' _ {}
+	xargs -P "$cores" -n 1 -I {} bash -c 'lookup_ip "$1"' _ {}
 }
 
 
 [ $# -eq 0 ] && { echo "Usage: $0 8.8.8.0/32 192.168.1.0/24"; exit 1; }
 
 # array of networks in CIDR notation
-cidr_networks=$@
+cidr_networks=("$@")
 
 # scope hack for thread safty
 export -f lookup_ip
